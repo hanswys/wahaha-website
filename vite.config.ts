838b5bb074189c99614fb188acc2ab1d91175d4e
hanswys <123027447+hanswys@users.noreply.github.com>
2025-02-22 00:12:11 -0600

    updated for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Remove the repository-specific base path for Netlify
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
